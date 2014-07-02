bb0ff71969b263b2f96ff4a0572a0cf11a886b22
ionaselig <you@example.com>
2014-07-02 18:06:22 +0100

    commit of directory version 7 with code TDDed and csv

--- directory 7/lib/directory.rb
+++ directory 7/lib/directory.rb
@@ -1,8 +1,35 @@
-require 'csv'
-
 def students
 	@students ||= []
 end
 
-	
+def print_welcome
+	puts 'Welcome to our Student Directory'
+end
 
+def get_name
+	puts 'Please enter a name'
+	gets.chomp
+end
+
+def add_student(student)
+	students << student
+end
+
+def students_to_csv(students)
+	CSV.open('./students.csv', 'wb') do |csv|
+		csv << students
+	end
+end
+
+def get_cohort
+	puts "Please enter #{test_students[:name]}'s cohort"
+	gets.chomp
+end
+
+def print_students
+	puts test_students
+end
+
+def print_footer
+	puts "We now have #{test_students.length} in our TDD directory"
+end
--- directory 7/spec/directory_spec.rb
+++ directory 7/spec/directory_spec.rb
@@ -1,33 +1,71 @@
 require 'directory'
+require 'csv'
 
+describe 'Student Directory' do
+	let (:test_students) { {name: 'Zoe', cohort: 'June'} }
 
-describe 'Student Directory' do 
-let (:student) {{:name =>'name', :cohort => 'cohort'}}
-
-	context 'upon opening' do
-		it 'starts with no students' do		
+	context 'on startup' do
+		it 'knows there are no students' do
 			expect(students).to be_empty
 		end
-	end
-end
-	context 'formats form header' do
-		it 'prints form by starting with header' do
-			expect (self).to receive(:puts).with("Welcome to student directory the TDD version") 
-		print_welcome
+		it 'prints the welcome message' do
+			expect(self).to receive(:puts).with('Welcome to our Student Directory')
+			print_welcome
+		end
+		it 'asks for the student\'s first name' do
+			expect(self).to receive(:puts).with('Please enter a name')
+			expect(self).to receive(:gets).and_return(test_students[:name])
+			expect(get_name).to eq test_students[:name]
+		end
+		it 'asks for the student\'s cohort' do
+			expect(self).to receive(:puts).with("Please enter #{test_students[:name]}'s cohort")
+			expect(self).to receive(:gets).and_return(test_students[:cohort])
+			expect(get_cohort).to eq test_students[:cohort]
 		end
 	end
-		it 'prompts input of student name'
-			expect(self).to receive(:puts).with('Please enter first name')
-			expect(self).to receive(:gets).and_return(student[:name])
-			expect(get_name).to eq student[:name] 
-		end 
-		it 'asks for student\'s cohort'	
-		expect(self).to receive(:puts).with('Please enter the student\s cohort')
-		expect(self).to receive(:gets).and_return(student[:cohort])	
-		expect(get_cohort).to eq student[:cohort]	 
+	context 'formats the inputted student' do
+	  # xit 'capitalizes the first letter of the name' do
+	  # 	expect(:name.regex \^*)
+	  # end
+		it 'puts the student into the array' do
+			add_student(test_students)
+			expect(students).to eq [test_students]
+		end
 
+		it 'saves the student to our file' do
+			dummy = double
+			expect(dummy).to receive(:<<).with(test_students)
+			expect(CSV).to receive(:open).with('./students.csv', 'wb').and_yield(dummy)
+			students_to_csv(test_students)
+		end
+	end
+
+	context 'it summarises student information' do
+		before do
+			add_student([test_students])
+		end
+
+		it 'can print a student on the screen' do
+			expect(self).to receive(:puts).with(test_students)
+			print_students
+		end
+
+		it 'prints the footer with the amount of students in our directory' do
+			expect(self).to receive(:puts).with("We now have #{test_students.length} in our TDD directory")
+			print_footer
+		end
+	end
+
+	context 'it can handle multiple students' do
+		let (:test_students) { [{name: 'One', cohort: 'October'}, {name: 'Two', cohort: 'May'}, {name: 'Three', cohort: 'Jan'}] }
+
+		before do
+			add_student([test_students])
+		end
+
+		it 'prints the list of students on the screen' do
+			expect(self).to receive(:puts).with(test_students)
+			print_students
+		end
+	end
 end
-
-
-
-
