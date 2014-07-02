00c81e5ce5cdef3be71ad17fcb7f26503683ffd4
ionaselig <you@example.com>
2014-07-02 19:08:18 +0100

    updated README and directory code looks more like a program than a list of defined variables

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
-#**Student Directory** 
-	
-###HOW TO USE
+Student Directory
+=================
+
+First project at Makers Academy is to create a list of students on our course, and handle that data.
+
+### Day 1 - directory.rb
+First attempt at coding with Nikesh (@nikeshashar). Struggled initally with Github remotes. subl command not responsive. Using a list of students and their hobbies we input them into an array. We try a few different ways of doing this as we progress through the lesson. Interesting talk from Nicola at ThoughtWorks.
+
+### Day 2 - directory2.rb and exercises
+Promiscuously paring away after Catharina (@CatharinaLD) and Nikesh, today I'm paired with Jamie (@jamieallen59). We start again, using the best of both our programmes from yesterday. Discover the joys of Ruby Docs and Stack Overflow. We crack on with the exercises that require our array prints students via various filters and elminates typos by defining constraints on possible responses.
+
+### Day 3 - exercises 
+Day 3 coding with Charlie (@Egalitea-leaves)! We explore file saving and loading, including going on a slight tangent adding functionality that checks if a file exists before trying to load / save. We add an interactive menu, prompting users to choose what they want to do. We explore saving the data to a csv file. Along the way we constantly refactor our code to make it as neat as possible. We touch on making a version that uses the CSV library to explore both.
+
+### Day 4 - exercises
+Day 4 with Pete. Talk from Richard at Just Giving.
+
+### Day 5 - directory 6
+Day 4 Largely missed between doctors and graduation. As a result I enjoyed my first programing threesome with Alex (@BobRazowski) and Jenny (@thejennywang). Introduction to TDD - Test Driven Development from Sam and started to refactor the code.
+
+### Day 6 - x
+dmissed due to uni graduation
+
+
+###Day 7 - directory 7
+TDD is here to stay, this time with Zoe (@zoebryant). Started from scratch and built up our tests again, who filled me in on checking CSV files with doubles which had been rather skipped over on Day 3. Accidentally overwrote ~/.bash-profile which removed rvm and rspec which held us up considerably but understood the reinstallation and commands in CLI much more by being forced to repeat them. Tried out vim and nano too. Though our rspec is pretty banging the program isn't on par with the spec file and as such we were testing short sightedly. Working on it now to get it back up to scratch.
+
+
+How to use
+----------
 ```shell
 ruby directory.rb
-'''
+```
+(directory[DAY NUMBER].rb)
--- exercises/exercise 8.rb
+++ exercises/exercise 8.rb
@@ -1,60 +1,113 @@
 
-def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "--------------"
+@students = []
+
+def run_program
+	input_students
+	print_header
+	print_names
+	students_with_firstletter_a
+	students_with_namelength_12
+	sorted_cohorts
+	print_footer
 end
 
-def print(students)
+def print_header
+	puts "The students of Makers Academy".center(80)
+	puts "--------------".center(80)
+end
+
+def print_names
 	student_counter = 0
-	while student_counter < students.count do
+	while student_counter < @students.count do
 		# puts "at this point the counter is #{student_counter}"
-		puts "#{students[student_counter][:name]} (#{students[student_counter][:cohort]} cohort) who is #{students[student_counter][:age]} years old and likes #{students[student_counter][:hobby]}".center(80) 
+		puts "#{student_counter + 1}) #{@students[student_counter][:name]} (#{@students[student_counter][:cohort]} cohort), is #{@students[student_counter][:age]} years old and likes #{@students[student_counter][:hobby]}".center(80)
 		student_counter = student_counter + 1
+		# if student[:name].start_with?("A")
+		# if student[:name].length < 12
 	end
 end
-
-
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
-end
-
-months = %w[january, January, February, february, March, march, April, april, May, may, June, june, July, july, August, august, September, september, October, october, November, november, December, december]
 
 def input_students
-	puts "Please enter the names, cohorts, hobbies, ages of the students respectively"
-	puts "To finish, just hit return twice"
-	students = []
+	puts "Enter the name, cohort, age and hobby of each of the Makers Academy students for the student directory."
+	puts "When you're done, just hit return twice"
+	# get the first name
 	puts "First name: "
 	name = gets.chomp.capitalize
+	# while the name is not empty, repeat this code
 	while !name.empty? do
-		puts "Cohort: "
-		if cohort != months[0..23] 
-			puts "Error, please return cohort month"
-			#embed loop back to line 30
-				else (months[0..23]):true | gets.chomp.capitalize|
-				end
+		# && !age.empty? && !hobbies.empty? 
+		puts "Enter the month (in numbers) of your cohort. e.g. 4 = April."
+		cohort = gets.chomp
+
+		if cohort.empty?
+			cohort = :unkown
+		end
+
+		cohort = (Time.new(Time.now.year, cohort.to_i).strftime ("%B").to_sym
+
+=begin cohort = gets.chomp.capitalize.to_sym
+			if cohort.empty?
+				cohort = :unkown
+			end
+=end
+
+		# asking for age number value between 0-90	
 		puts "Age: "
-		if age == integer:true | gets.chomp|
-			else puts "Error, please input age in numerical values"	
-			#embed loop back to line 36	
-			end 	
+		age = gets.chomp
+			while (age.to_i == 0 or age.to_i < 0 or age.to_i > 90)
+				puts "Please enter a REAL age (in numbers!)"
+				age = gets.chomp
+			end
+
 		puts "Hobby: "
 		hobbies = gets.chomp
-		if hobbies.empty? 
-		p hobbies = "unknown" 
-
-		students << {:name => name, :cohort => cohort, :hobby => hobbies, :age => age}
-		puts "There are #{students.length} entered. Now enter the next one."
+		# add the student hash to the array
+		@students << {name: name, cohort: cohort, hobby: hobbies, age: age}
+			if @students.length == 1
+				puts "Now we have #{@students.length} student. Enter the next one."
+			else 
+				puts "Now there are #{@students.length} students. Enter the next one..."
+		end
 		# get another name from the user
 		puts "First name: "
-		name = gets.chomp
-		
+		name = gets.chomp.capitalize 
 	end
 	# return the array of students
-	students
+	#students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def sorted_cohorts
+	puts "\n "
+	puts "Here are the students sorted by cohort".center(80)
+	puts "--------------".center(80)
+	@students.sort_by! {|sorted_students| sorted_students[:cohort]}
+	print_names
+end
+
+
+def students_with_firstletter_a
+	@students.each do |student|
+	if student[:name].start_with?("A" , "a") #or student[:name].start_with?("a")
+		puts "\n "
+		puts "The students who have names beginning with the letter A are: #{student[:name]}"
+		end
+	end
+end
+
+def students_with_namelength_12
+	@students.each do |student|
+		if student[:name].length > 12
+			puts "\n "
+			puts "The students with freakishly long first names are: #{student[:name]}"
+		end
+	end
+end
+
+def print_footer
+	puts "\n "
+	puts "So overall, we have #{@students.length}... questionable students.".center(80)
+	puts "\n "
+end
+
+run_program
